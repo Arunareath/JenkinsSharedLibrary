d47ec5186752cc9090f9eb2a91ca55616ca7ebe5
Arunareath <55378855+Arunareath@users.noreply.github.com>
2024-08-30 01:53:25 +0530

    Update k8sPodTemplate.groovy

--- vars/k8sPodTemplate.groovy
+++ vars/k8sPodTemplate.groovy
@@ -1,27 +1,27 @@
-def call(Map args = [:]) {
-    return podTemplate(
-        yaml: '''
-        apiVersion: v1
-        kind: Pod
-        spec:
-          containers:
-          - name: maven
-            image: maven:alpine
-            command:
-            - cat
-            tty: true
-          - name: docker
-            image: docker:latest
-            command:
-            - cat
-            tty: true
-            volumeMounts:
-             - mountPath: /var/run/docker.sock
-               name: docker-sock
-          volumes:
-          - name: docker-sock
-            hostPath:
-              path: /var/run/docker.sock    
-        '''
-    ) 
+def call() {  
+   return [  
+      yaml: '''  
+      apiVersion: v1  
+      kind: Pod  
+      spec:  
+       containers:  
+       - name: maven  
+        image: maven:alpine  
+        command:  
+        - cat  
+        tty: true  
+       - name: docker  
+        image: docker:latest  
+        command:  
+        - cat  
+        tty: true  
+        volumeMounts:  
+         - mountPath: /var/run/docker.sock  
+          name: docker-sock  
+       volumes:  
+       - name: docker-sock  
+        hostPath:  
+          path: /var/run/docker.sock    
+      '''  
+   ]  
 }
